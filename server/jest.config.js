f87e36737b9081a02e65afd2c263feaafa540c14
Ashrith Kavva <ashrithkavva@gmail.com>
2025-07-15 10:12:00 -0400

    Initial project setup and repo structure

--- server/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '\\.integration\\.test\\.ts$'],
-  verbose: true,
-  testTimeout: 15000,
-};
--- server/src/context.ts
+++
@@ -1,6 +0,0 @@
-import { Loaders } from './loaders';
-
-export type GraphQLContext = {
-  requestId: string;
-  loaders: Loaders;
-};
